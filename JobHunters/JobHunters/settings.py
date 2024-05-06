93a97ef1fd99b42ce2594b5ee4bca609e246f06b
Hrafnkellh <hrafnkellh21@ru.is>
2024-05-06 11:14:38 +0000

    Added templates and indices

--- JobHunters/JobHunters/settings.py
+++ JobHunters/JobHunters/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'JobHunters.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,7 +80,8 @@ DATABASES = { 'default': { 'ENGINE': 'django.db.backends.postgresql',
                           'PASSWORD': '1WK8otByxh',
                           'HOST': 'verklegt-namskeid-ii.northeurope.cloudapp.azure.com',
                           'PORT': '5432', 
-                          } }
+                          }
+            }
 
 
 # Password validation
