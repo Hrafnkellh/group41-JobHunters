a807252b7066d56f2e1a531c22c3c214d0d356a9
Hrafnkellh <hrafnkellh21@ru.is>
2024-05-06 10:16:58 +0000

    Added postgres database

--- JobHunters/JobHunters/settings.py
+++ JobHunters/JobHunters/settings.py
@@ -73,12 +73,13 @@ WSGI_APPLICATION = 'JobHunters.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = { 'default': { 'ENGINE': 'django.db.backends.postgresql',
+                          'NAME': 'postgres',
+                          'USER': 'vln2_assignment_groups_41_user',
+                          'PASSWORD': '1WK8otByxh',
+                          'HOST': 'verklegt-namskeid-ii.northeurope.cloudapp.azure.com',
+                          'PORT': '5432', 
+                          } }
 
 
 # Password validation
