9a9b5ce57b01951751c0e7fda5c435b6c664b0bb
Hrafnkellh <hrafnkellh21@ru.is>
2024-05-06 11:04:40 +0000

    Added models according to class diagram

--- JobHunters/Jobs/models.py
+++ JobHunters/Jobs/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class JobListing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True)
+    salary = models.CharField(max_length=255)
+    requirements = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+    
+class JobApplication(models.model):
+    title = models.CharField(max_length=255)
+    cover_letter = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+
+    job_listing = models.ForeignKey(JobListing, on_delete=models.CASCADE, blank=False)
+
+    def __str__(self):
+        return self.title
+    
+class Interview(models.model):
+    date_time = models.DateTimeField()
+
+    job_listing = models.ForeignKey(JobListing, on_delete=models.CASCADE, blank=False)
+    job_application = models.ForeignKey(JobApplication, on_delete=models.CASCADE, blank=False)
+
+    def __str__(self):
+        return f'{JobListing.title}: {JobApplication.title}
+        {self.date_time}'
