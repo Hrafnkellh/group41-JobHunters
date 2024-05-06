17dddc1a6b0441a5c0f1bf7180762ebe7ab55e3a
Hrafnkellh <hrafnkellh21@ru.is>
2024-05-06 15:07:39 +0000

    Added user model basics

--- JobHunters/Users/models.py
+++ JobHunters/Users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class JobSeeker(User):
+    resume = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class Employer(User):
+    esg_rating = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
