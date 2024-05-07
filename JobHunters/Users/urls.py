7abe113ef78ebec607a32d7b5c814fc306c629eb
gunnimikki <gunnimikki@gmail.com>
2024-05-07 13:39:59 +0000

    Fixed navigation
    uses correct reference now

--- JobHunters/Users/urls.py
+++ JobHunters/Users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('/sign_up', views.sign_up_index, name='sign_up')
+    path('', views.index, name='user_index'),
+    path('sign_up', views.sign_up_index, name='sign_up_index')
 ]
