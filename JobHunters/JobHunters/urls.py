04010fc29fd448a1a35e7a0d62ace7f9dc3afd6a
Hrafnkellh <hrafnkellh21@ru.is>
2024-05-07 10:44:52 +0000

    added jobs.urls to base path

--- JobHunters/JobHunters/urls.py
+++ JobHunters/JobHunters/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('Jobs.urls')),
     path('jobs/', include('Jobs.urls')),
     path('users', include('Users.urls'))
 ]
