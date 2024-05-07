46eaff85acdde56d505493846a60df224be133e1
Davygod <davidthorfa@gmail.com>
2024-05-07 13:45:38 +0000

    Setti upp forsíðu

--- JobHunters/Jobs/urls.py
+++ JobHunters/Jobs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='jobs_index'),
+    path('', views.frontpage, name='frontpage'),
 ]
--- JobHunters/Jobs/views.py
+++ JobHunters/Jobs/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'Jobs/index.html' )
+
+def frontpage(request):
+    return render(request, 'Jobs/frontpage.html')
