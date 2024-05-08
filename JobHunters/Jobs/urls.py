05c891e6da2a4b750b2a25fc4ef803f9e271f9da
Davygod <davidthorfa@gmail.com>
2024-05-08 21:58:30 +0000

    Villtur með tengla

--- JobHunters/Jobs/urls.py
+++ JobHunters/Jobs/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
+    path('', views.frontpage, name='faq'),
+    path('', views.frontpage, name='aboutus'),
+    path('', views.frontpage, name='jobtips'),
 ]
