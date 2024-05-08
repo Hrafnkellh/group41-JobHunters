2ebd3d99206143e7cef293ab14566c6640231fca
gunnimikki <gunnimikki@gmail.com>
2024-05-08 20:23:39 +0000

    Log in not quite done.
    close to completion with a working log in, biggest hurdle is how to get {{ form }} in log_in.html and sign_up.html working like in the candy queen examples

--- JobHunters/Users/urls.py
+++ JobHunters/Users/urls.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='user_index'),
     path('/sign_up', views.sign_up_index, name='sign_up_index'),
-    path('/log_in', views.log_in_index, name='log_in_index')
+    path('/log_in', views.log_in_index, name='log_in_index'),
+
+    path('/sign_up', views.register, name='Register_User'),
+    path('/log_in', LoginView.as_view(template_name='user/log_in.html'), name = 'Log_In_User'),
+    path('/log_out', LogoutView.as_view(next_page='log_in'), name='Logout_User'),
 ]
