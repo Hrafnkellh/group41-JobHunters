3f187fc99ff9a858322c7600cd70d60e7c69f40a
Hrafnkellh <hrafnkellh21@ru.is>
2024-05-10 10:50:12 +0000

    added rceommendation and experience models

--- JobHunters/Jobs/models.py
+++ JobHunters/Jobs/models.py
@@ -18,7 +18,12 @@ class JobApplication(models.Model):
     title = models.CharField(max_length=255)
     cover_letter = models.CharField(max_length=255)
     status = models.CharField(max_length=255)
-
+    full_name = models.CharField(max_length=255)
+    street_name = models.CharField(max_length=255)
+    house number = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    postal-code = models.CharField(max_length=255)
 
     job_listing = models.ForeignKey(JobListing, on_delete=models.CASCADE, blank=False)
     job_seeker = models.ForeignKey(JobSeeker, on_delete=models.CASCADE, blank=False)
@@ -34,3 +39,20 @@ class Interview(models.Model):
 
     def __str__(self):
         return f'{JobListing.title}: {JobApplication.title}\n{self.date_time}'
+
+class Experience(models.Model):
+    place_of_work = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    start_date = models.models.DateTimeField()
+    end_date = models.models.DateTimeField()
+
+    job_application = models.ForeignKey(JobApplication, on_delete=models.CASCADE, blank=False)
+
+class Recommendation(models.Model):
+    name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    may_be_contacted = models.BooleanField()
+    role = models.CharField(max_length=255)
+
+    job_application = models.ForeignKey(JobApplication, on_delete=models.CASCADE, blank=False)
