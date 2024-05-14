c0ab80f26a5467f67fd36464c80f8a912cdc2001
Hrafnkellh <hrafnkellh21@ru.is>
2024-05-14 12:07:11 +0000

    fixed naming scheme and style job detail site

--- JobHunters/Users/views.py
+++ JobHunters/Users/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from Users.forms.profile_form import ProfileForm
-from Users.models import Profile
+from Users.models import Employer, Profile
 
 # Create your views here.
 def index(request):
     return render(request, 'Users/index.html' )
 
-def log_in(request):
+def logIn(request):
     return render(request, 'Users/log_in.html' )
 
 def register(request):
@@ -22,7 +22,7 @@ def register(request):
     return render(request, 'Users/sign_up.html', {'form': UserCreationForm()})
 
 @login_required
-def profile_view(request):
+def profile(request):
     user = request.user
     if request.method == 'POST':
         form = ProfileForm(request.POST, instance=user)
@@ -35,3 +35,8 @@ def profile_view(request):
         form = ProfileForm(instance=user)
 
     return render(request, 'Users/profile.html', {'form': form})
+
+def employersDetails(request, id):
+    return render(request, 'Users.employer_details_site.html', context={
+        'employer': get_object_or_404(Employer, pk=id)
+    })
