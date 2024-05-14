d7630074b001b209bf40ccc6273a79954b9c519c
gunnimikki <gunnimikki@gmail.com>
2024-05-14 15:15:01 +0000

    profile updates

--- JobHunters/Users/forms/profile_form.py
+++ JobHunters/Users/forms/profile_form.py
@@ -4,9 +4,10 @@ from Users.models import Profile
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
-        exclude = ['id', 'user']
+        exclude = ['id', 'user_id', 'resume', 'user']
         widgets = {
             'name': widgets.TextInput(attrs={'class': 'form-control'}),
             'profile_image_path': widgets.FileInput(attrs={'class': 'form-control'}),
             'address': widgets.TextInput(attrs={'class': 'form-control'}),
+            'email': widgets.TextInput(attrs={'class': 'form-control'}),
         }
