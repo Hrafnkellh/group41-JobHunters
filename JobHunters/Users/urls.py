ed0ca04e5997035e3cbd2842791bed0fba9297f2
Hrafnkellh <hrafnkellh21@ru.is>
2024-05-15 13:15:58 +0000

    Application detail site started

--- JobHunters/Users/urls.py
+++ JobHunters/Users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('log_in', LoginView.as_view(template_name='Users/log_in.html'), name = 'log_in'),
     path('log_out', LogoutView.as_view(next_page = 'log_in'), name='log_out'),
     path('profile', views.profile, name='profile'),
-    path(route='employers/<int:id>', view=views.employerDetails, name='employer_details')
+    path(route='employers/<int:id>', view=views.employerDetails, name='employer_details'),
+    path(route='profile/<int:id>', view=views.applicationDetails, name='application_details')
 ]
