150ab485fab8382b2c92fd07178395c6f199363b
Davygod <davidthorfa@gmail.com>
2024-05-17 20:58:56 +0000

    Laga stafsetningarvillur og fjarlægja smá óþarfa kóðabyggingar

--- JobHunters/Jobs/urls.py
+++ JobHunters/Jobs/urls.py
@@ -9,9 +9,4 @@ urlpatterns = [
     path('employers', views.employers, name='employers'),
     path(route='<int:id>', view=views.jobDetails, name='jobDetails'),
     path(route='<int:id>/jobApplication/', view=views.jobApplication, name='job_application'),
-    
-    #path(route='<int:id>/jobApplication/', view=views.JAP_1_1, name='jobApp_1_1'),
-    #path(route='<int:id>/jobApplication/', view=views.jobApplicationPage1Contact, name='jobApplication'),
-    #path(route='<int:id>/jobApplicationn/', view=views.jobApplicationPage2Cover,  name='jobApplicationn'),
-    #path(route='<int:id>/jobApplication3/', view=views.jobApplicatonPage3,  name='jobApplication3')
 ]
--- JobHunters/Jobs/views.py
+++ JobHunters/Jobs/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from Users.models import Employer
 from Jobs.models import Experience, JobListing, JobApplication, JobSeeker, Recommendation
